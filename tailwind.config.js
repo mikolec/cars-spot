7837686c8be6886baa4beecb029398adc0226ef5
Mikołaj Lechtański <mikolaj.lechtanski@accenture.com>
2024-09-15 19:22:44 +0200

    Initial commit, after completing the base of the project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#0147FF",
+        darkgray: "#282828",
+      },
+    },
+  },
+  plugins: [],
+};
